0b85ac58bcfe4714e815485452c5aed77609bbbd
Daniel Quinlan <daniel@chaosengine.net>
2019-10-17 07:48:53 -0700

    update URI for goimports

--- install_go_tools.sh
+++ install_go_tools.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 go get -v -u github.com/nsf/gocode
-go get -v -u code.google.com/p/go.tools/cmd/goimports
+go get -v -u golang.org/x/tools/cmd/goimports
 go get -v -u github.com/jstemmer/gotags
